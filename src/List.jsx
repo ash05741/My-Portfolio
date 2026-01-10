798833e56b41dc41b101b3546f3a34e1b6f23a97
ash05741 <asmitshukla0505@gmail.com>
2026-01-11 01:33:23 +0530

    Delete src/List.jsx

--- src/List.jsx
+++
@@ -1,7 +0,0 @@
-function List(){
-    const fruits = ["apple", "mango","banana"]
-    fruits.sort();
-    const listitems = fruits.map(fruits => <li>{fruits}</li>)
-    return(<ol>{listitems}</ol>)
-}
-export default List; 
